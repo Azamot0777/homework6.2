b1745cf67551eb41cee1bfa4ea9919c26f8d2f3d
Heisenberg <Persey_98@list.ru>
2020-04-05 21:16:26 +0500

    created classes (Book , Author, Content, Title,) and Method Show

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Homework6_2
+{
+    class Book
+    {
+        public Title name = new Title();
+        public Author author = new Author();
+        public Content content = new Content();
+        public void Show()
+        {
+
+        }
+    }
+
+    class Title
+    {
+        public void Show()
+        {
+
+        }
+    }
+
+    class Author
+    {
+        public void Show()
+        {
+
+        }
+    }
+    class Content
+    {
+        public void Show()
+        {
+
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book n = new Book();
+            Title g = new Title();
+            Author h = new Author();
+            Content m = new Content();
+
+        }
+    }
+}
